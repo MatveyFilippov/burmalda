d4da24e67c90f92b180b343e9ff26265560a39ae
alinjia <114215696+alinjia@users.noreply.github.com>
2023-06-18 22:10:18 +0300

    fix bugs

--- Application.java
+++ Application.java
@@ -1,9 +1,8 @@
-package samoe_kreativnoe.burmolda;
+package samoe_kreativnoe.burmalda;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
@@ -11,7 +10,7 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
-        stage.setTitle("Burmolda");
+        stage.setTitle("Burmalda");
         stage.setScene(scene);
         stage.show();
     }
